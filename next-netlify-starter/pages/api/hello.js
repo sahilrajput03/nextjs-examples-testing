c033f1b2694f310b6a2dea2fd9383e25e08a77ec
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-30 17:44:15 +0530

    Add next netlify-starter repo.

---
+++ next-netlify-starter/pages/api/hello.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {log} from "./helpers";
+
+export default (req, res) => {
+  log({body: req.body});
+
+  res.status(200).json({name: "Sahil Rajput", ...req.body});
+};
