e85bde51724d05715956e4d066f01d97d31adba1
Sahil Rajput <sahilrajput03@gmail.com>
2022-09-27 00:03:05 +0530

    Add tests watcher for typescript project, works awesome

---
+++ api-testing-typescript-with-flash/README.md
@@ -0,0 +1,16 @@
+# Learn nextjs api testing
+
+## Typescript Guide
+
+You need to run a supportive tsc compiler for tests to run in a side-terminal (i.e., `test-watch-compiler`) and it just works, yo!!
+
+
+`node-mocks-http`
+- Since npm has significant download so its support must good.
+- [github 700*](https://github.com/howardabrams/node-mocks-http), [npm 400K↓](https://www.npmjs.com/package/node-mocks-http)
+-  **Awesome Article**: [How to Unit Test Next.js API Routes with TypeScript](https://www.paigeniedringhaus.com/blog/how-to-unit-test-next-js-api-routes-with-typescript)
+
+
+`next-test-api-route-handler` or `ntarh`
+  - `ntarh`'s author is too rigid about using `node-fetch` for calling the api and doesn't allow flexibility to use `axios` instead of that. Visit [this discussion](https://github.com/Xunnamius/next-test-api-route-handler/discussions/172) to know about this.
+  - [npm](https://www.npmjs.com/package/next-test-api-route-handler), [github](https://github.com/Xunnamius/next-test-api-route-handler)
---
+++ api-testing-typescript-with-flash/__tests__/1.test.ts
@@ -0,0 +1,45 @@
+// @ts-nocheck
+/**
+ * @jest-environment node
+ */
+// MOTIVATION, INSPIRATION: https://www.paigeniedringhaus.com/blog/how-to-unit-test-next-js-api-routes-with-typescript
+import {createMocks} from 'node-mocks-http'
+import helloApi from '../pages/api/hello'
+import {expect} from 'expect'
+
+connectToDb(async () => {})
+
+const helloApiCaller = async (cb) => {
+	const {req, res} = createMocks({method: 'GET'})
+	await cb(req) // Just making sure that in future one may need to so some async tasks in here as well  ~Sahil
+	await helloApi(req, res)
+	return {req, res}
+}
+
+describe('/api/hello', () => {
+	const authToken = process.env.AUTH_TOKEN
+
+	test('should return a successful response from Notehub', async () => {
+		const payload = {
+			name: 'Sahil',
+			age: 13,
+		}
+
+		const {req, res} = await helloApiCaller((req) => {
+			req.headers = {
+				'Content-Type': 'application/json',
+				'X-SESSION-TOKEN': authToken,
+			}
+			req.query = {gatewayID: 'some_value'}
+			req.body = payload
+		})
+
+		const responseBody = res._getJSONData()
+		expect(responseBody.name).toBe(payload.name)
+		expect(responseBody.age).toBe(payload.age)
+		//
+		expect(res.statusCode).toBe(200)
+		expect(res.getHeaders()).toEqual({'content-type': 'application/json'})
+		expect(res.statusMessage).toEqual('OK')
+	})
+})
